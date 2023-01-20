a57c09aab77299465148ab0af08df0c7d8039936
manunovoa <manunovoa0415@gmail.com>
2023-01-20 09:44:00 -0500

    se agrego el dump sql de la base de datos

--- clientesfidelizacion.sql
+++ clientesfidelizacion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 20-01-2023 a las 15:16:08
+-- Tiempo de generación: 20-01-2023 a las 15:42:40
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.1.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `clientesfidelizacion`
 --
+CREATE DATABASE IF NOT EXISTS `clientesfidelizacion` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `clientesfidelizacion`;
 
 -- --------------------------------------------------------
 
@@ -27,12 +29,14 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `ciudades`
 --
 
-CREATE TABLE `ciudades` (
+CREATE TABLE IF NOT EXISTS `ciudades` (
   `id` int(11) NOT NULL,
   `ciudad` varchar(70) NOT NULL,
   `idDepartamentos` int(11) NOT NULL,
   `createdAt` date NOT NULL DEFAULT current_timestamp(),
-  `updatedAt` date NOT NULL DEFAULT current_timestamp()
+  `updatedAt` date NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `FKdepartamento` (`idDepartamentos`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -62,8 +66,8 @@ INSERT INTO `ciudades` (`id`, `ciudad`, `idDepartamentos`, `createdAt`, `updated
 -- Estructura de tabla para la tabla `cliente`
 --
 
-CREATE TABLE `cliente` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `documento` varchar(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellido` varchar(50) NOT NULL,
@@ -73,7 +77,11 @@ CREATE TABLE `cliente` (
   `idMarca` int(11) NOT NULL,
   `idPaises` int(11) NOT NULL,
   `createdAt` date NOT NULL,
-  `updatedAt` date NOT NULL
+  `updatedAt` date NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `FKtipoDocumento` (`idTipoDocumento`),
+  UNIQUE KEY `FKmarca` (`idMarca`),
+  UNIQUE KEY `FKpaises` (`idPaises`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -82,12 +90,14 @@ CREATE TABLE `cliente` (
 -- Estructura de tabla para la tabla `departamentos`
 --
 
-CREATE TABLE `departamentos` (
+CREATE TABLE IF NOT EXISTS `departamentos` (
   `id` int(11) NOT NULL,
   `departamento` varchar(60) NOT NULL,
   `idPaises` int(11) NOT NULL,
   `createdAt` date NOT NULL DEFAULT current_timestamp(),
-  `updatedAt` date NOT NULL DEFAULT current_timestamp()
+  `updatedAt` date NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `FKpais` (`idPaises`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -108,12 +118,13 @@ INSERT INTO `departamentos` (`id`, `departamento`, `idPaises`, `createdAt`, `upd
 -- Estructura de tabla para la tabla `marcas`
 --
 
-CREATE TABLE `marcas` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `marcas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `marca` varchar(50) NOT NULL,
   `createdAt` date NOT NULL DEFAULT current_timestamp(),
-  `updatedAt` date NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `updatedAt` date NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `marcas`
@@ -133,11 +144,12 @@ INSERT INTO `marcas` (`id`, `marca`, `createdAt`, `updatedAt`) VALUES
 -- Estructura de tabla para la tabla `paises`
 --
 
-CREATE TABLE `paises` (
+CREATE TABLE IF NOT EXISTS `paises` (
   `id` int(11) NOT NULL,
   `pais` varchar(70) NOT NULL,
   `createdAt` date NOT NULL DEFAULT current_timestamp(),
-  `updatedAt` date NOT NULL DEFAULT current_timestamp()
+  `updatedAt` date NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -155,12 +167,13 @@ INSERT INTO `paises` (`id`, `pais`, `createdAt`, `updatedAt`) VALUES
 -- Estructura de tabla para la tabla `tipoDocumentos`
 --
 
-CREATE TABLE `tipoDocumentos` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `tipoDocumentos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `tipoDocumento` varchar(20) NOT NULL,
   `createdAt` date NOT NULL DEFAULT current_timestamp(),
-  `updatedAt` date NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `updatedAt` date NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `tipoDocumentos`
@@ -170,73 +183,6 @@ INSERT INTO `tipoDocumentos` (`id`, `tipoDocumento`, `createdAt`, `updatedAt`) V
 (1, 'T.I', '2023-01-15', '2023-01-15'),
 (2, 'Cédula', '2023-01-15', '2023-01-15'),
 (3, 'Pasaporte', '2023-01-15', '2023-01-15');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `ciudades`
---
-ALTER TABLE `ciudades`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FKdepartamento` (`idDepartamentos`) USING BTREE;
-
---
--- Indices de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `FKtipoDocumento` (`idTipoDocumento`),
-  ADD UNIQUE KEY `FKmarca` (`idMarca`),
-  ADD UNIQUE KEY `FKpaises` (`idPaises`);
-
---
--- Indices de la tabla `departamentos`
---
-ALTER TABLE `departamentos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FKpais` (`idPaises`);
-
---
--- Indices de la tabla `marcas`
---
-ALTER TABLE `marcas`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `paises`
---
-ALTER TABLE `paises`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `tipoDocumentos`
---
-ALTER TABLE `tipoDocumentos`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `marcas`
---
-ALTER TABLE `marcas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT de la tabla `tipoDocumentos`
---
-ALTER TABLE `tipoDocumentos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
